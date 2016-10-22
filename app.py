b3a57d1e9485e729b4ba58efa30c53ad42e3b005
senandtihiro <760584800@qq.com>
2016-10-22 19:21:55 +0800

    修改与todo相关

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ manager = Manager(app)
 
 
 def register_routes(app):
-    from routes.todo import main as routes_todo
     from routes.node import main as routes_node
     from routes.topic import main as routes_topic
     from routes.user import main as routes_user
@@ -44,7 +43,6 @@ def register_routes(app):
     app.register_blueprint(routes_weibo, url_prefix='/weibo')
     app.register_blueprint(routes_comment, url_prefix='/comment')
     app.register_blueprint(routes_api, url_prefix='/api')
-    app.register_blueprint(routes_todo, url_prefix='/todo')
 
 def configure_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
