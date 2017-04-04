9d529aac35c7709861ace5e6525b2db6fe2005ec
senandtihiro <760584800@qq.com>
2017-04-04 23:44:29 +0800

    修改wsgi文件

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,14 @@
-import app as bbs
+# import app as bbs
+#
+# app = bbs.configured_app()
+import sys
+from os.path import abspath
+from os.path import dirname
+import app
 
-app = bbs.configured_app()
+
+sys.path.insert(0, abspath(dirname(__file__)))
+application = app.app
 
 # gunicorn appcorn:app
 # nohup gunicorn -b '0.0.0.0:80' appcorn:app &
