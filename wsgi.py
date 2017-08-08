c576d87c9030597fe26bdbaf7bf8af185127d8b4
何玉莲 <760584800@qq.com>
2017-08-08 22:47:12 +0800

    …

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
-import sys
-from os.path import abspath
-from os.path import dirname
-import app
-
-
-sys.path.insert(0, abspath(dirname(__file__)))
-application = app.app
-
-# import app as bbs
+# import sys
+# from os.path import abspath
+# from os.path import dirname
+# import app
 #
-# app = bbs.configured_app()
+#
+# sys.path.insert(0, abspath(dirname(__file__)))
+# application = app.app
+
+import app as bbs
+
+app = bbs.configured_app()
 
 # gunicorn appcorn:app
 # nohup gunicorn -b '0.0.0.0:5001' wsgi:app &
