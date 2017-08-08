886e9b05ef08439b5c1fd25955a9f781f1167b67
何玉莲 <760584800@qq.com>
2017-08-08 23:50:02 +0800

    …

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python3
-
-# import sys
-# from os.path import abspath
-# from os.path import dirname
-# import app
-#
-#
-# sys.path.insert(0, abspath(dirname(__file__)))
-# application = app.app
-
 import app as bbs
 
 app = bbs.configured_app()
