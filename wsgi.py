088f6b92cdba95e6a657f1593c86f4b5f65c2194
何玉莲 <760584800@qq.com>
2017-08-08 22:41:23 +0800

    modify config

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,19 @@
-import app as bbs
+#!/usr/bin/env python3
 
-app = bbs.configured_app()
+import sys
+from os.path import abspath
+from os.path import dirname
+import app
+
+
+sys.path.insert(0, abspath(dirname(__file__)))
+application = app.app
+
+# import app as bbs
+#
+# app = bbs.configured_app()
 
 # gunicorn appcorn:app
-# nohup gunicorn -b '0.0.0.0:80' appcorn:app &
+# nohup gunicorn -b '0.0.0.0:5001' wsgi:app &
 
 # wsgi
